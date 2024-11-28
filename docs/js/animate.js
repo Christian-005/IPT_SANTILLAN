5319a010895fa0bb18d0fc9e2ab47e3366a23991
Christian-005 <Chritian-005>
2024-11-28 14:01:39 +0800

    MY PORTFOLIO

---
+++ docs/js/animate.js
@@ -0,0 +1,14 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Backend-Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Click my Resume For more Information About Me", "Click my Resume For more Information About Me"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
